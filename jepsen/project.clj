b41cbdda86efb1ba989cb7b555028925f58482b8
amakhov <amakhov@mirantis.com>
2026-03-30 10:21:20 +0300

    Jepsen tests

---
+++ jepsen/project.clj
@@ -0,0 +1,21 @@
+(defproject jepsen.strata "0.1.0-SNAPSHOT"
+  :description "Jepsen linearizability tests for Strata"
+  :license     {:name "Apache-2.0"}
+  :main        jepsen.strata.core
+  :source-paths ["src"]
+
+  :dependencies
+  [[org.clojure/clojure  "1.11.3"]
+   [jepsen               "0.3.5"]
+   ;; jetcd: official Java etcd v3 client used to drive strata's etcd API.
+   [io.etcd/jetcd-core   "0.7.7"]]
+
+  :jvm-opts
+  ["-Djava.awt.headless=true"
+   ;; jetcd uses Netty which reflectively accesses JDK internals on Java 17+.
+   "--add-opens=java.base/java.lang=ALL-UNNAMED"
+   "--add-opens=java.base/java.nio=ALL-UNNAMED"
+   "--add-opens=java.base/sun.nio.ch=ALL-UNNAMED"]
+
+  :profiles
+  {:dev {:dependencies [[org.clojure/tools.namespace "1.5.0"]]}})
